6900a8c1b0b0642d0806a992e489faf4193f5516
Jean-Chrystophe.Fréreault <1969699@cegepmontpetit.ca>
2020-11-20 17:17:11 -0500

    serpen début création jeux (var unePartie créer)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //le jeu
+    class Jeu{
+        constructor() {
+
+            console.log('création jeu')
+
+        }
+
+
+
+    }
+
+
+    //le serpent
+
+    class Serpent{
+
+        constructor() {
+
+            console.log('création serpent')
+
+        }
+
+
+    }
+
+
+    //la pomme
+    class Pomme{
+
+        constructor() {
+
+            console.log('création pomme')
+
+        }
+
+
+    }
+
+
+    var unePartie = new Jeu();
+
+
+
 
 });
